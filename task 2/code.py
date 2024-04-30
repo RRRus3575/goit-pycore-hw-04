5dc58502d8d59ce45be0e973eccb2128182efa76
Ruslan <ruslan.grudistov@gmail.com>
2024-04-30 16:16:22 +0300

    2

--- task 2/code.py
+++ task 2/code.py
@@ -8,15 +8,16 @@ def get_cats_info(path):
             cats_list = []
 
             for line in file:
-                result = line.split(',')
+                result = line.strip().split(',')
                 cat_dict = {
                     'id': result[0],
                     'name': result[1],
-                    'age': result[2].rstrip('\n')
+                    'age': int(result[2])
                 }
                 cats_list.append(cat_dict)
 
             return cats_list
+        
 
 
     except Exception as e:
